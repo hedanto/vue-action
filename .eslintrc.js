b6901e12a37e75d5e599655686ec3917a71808a4
tim <642752012@qq.com>
2017-01-03 18:29:54 +0800

    router control

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,11 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
+    // 箭头函数参数可以不要括号
     'arrow-parens': 0,
-    // allow async-await
+    // 不强制生成器函数*的前后空格
     'generator-star-spacing': 0,
-    // allow debugger during development
+    // 开发模式可以使用debugger，生产模式不可以
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     // 需要分号结尾
     'semi': ['error', 'always']
